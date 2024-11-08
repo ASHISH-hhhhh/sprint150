47c41607bc7f5a7ea9a6ed9db942667d63ce50d4
Ashish Biradar <127400020+ASHISH-hhhhh@users.noreply.github.com>
2024-11-08 13:07:04 +0530

    Update sprint1-50 Added the solution for sum of odd numbers in a range

---
+++ sprint1-50/22.sumOddInRange.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int sumOddInRange(int a, int b) {
+    int sum=0;
+    for(int i=a; i<=b; i++) {
+        if(i%2!=0) {
+            sum=sum+i;
+        }
+    }
+    return sum;
+}
+
+int main()
+{
+    cout << "**********************Sprint150!*****************************" << endl;
+    int a, b;
+    cout<<"Enter the Upper range :" ;
+    cin>>a;
+    cout<<"Enter the Lower range :" ;
+    cin>>b;
+      cout<<sumOddInRange(a, b);
+    return 0;
+}
