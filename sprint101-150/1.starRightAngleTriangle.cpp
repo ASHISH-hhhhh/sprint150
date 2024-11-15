b84740c4b99a731f2ddbe1d4a4da6d6c13957994
Ashish Biradar <127400020+ASHISH-hhhhh@users.noreply.github.com>
2024-11-15 14:37:14 +0530

    Create 1.starRightAngleTriangle.cpp Adde the solution for  a Right-Angle Triangle of Stars

---
+++ sprint101-150/1.starRightAngleTriangle.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int row;
+    cin >> row;
+    for (int i = 1; i <= row; i++) {
+        for (int c = 1; c <= i; c++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
