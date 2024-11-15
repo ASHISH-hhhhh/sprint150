3ad9d2b07b9a0dc1d40e75d51e8a34aa9a0bbdf2
Ashish Biradar <127400020+ASHISH-hhhhh@users.noreply.github.com>
2024-11-15 14:51:50 +0530

    Create 4.diamondStars.cpp Adde the solution for diamond star pattern

---
+++ sprint101-150/4.diamondStars.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int row;
+    cin >> row;
+    for (int i = 1; i <= row; i++) {
+        for(int sp=1;sp<=row-i;sp++){
+            cout<<" ";
+        }
+        for (int c = 1; c <=i*2-1; c++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for(int i=row-1;i>=1;i--){
+        for(int sp=1;sp<=row-i;sp++){
+            cout<<"-";
+        }
+        for(int c=1;c<=i*2-1;c++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
