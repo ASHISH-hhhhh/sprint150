07302e4a4ae2d9eba8b8f02e603ea64089914574
Ashish Biradar <127400020+ASHISH-hhhhh@users.noreply.github.com>
2024-11-16 08:26:12 +0530

    Update sprint101-150 Added the solution for increasing star pattern by *2

---
+++ sprint101-150/14.starPatternWithIncreasing*2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int row;
+    cout<<"Enter the row number :" ;
+    cin>>row;
+    for(int i=1; i<=row; i++) { 
+        for(int c=1; c<=i*2-1; c++) {
+             cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
