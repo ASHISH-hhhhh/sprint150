3e52e44e12653aa9fb04b16e01714e6225572066
Ashish Biradar <127400020+ASHISH-hhhhh@users.noreply.github.com>
2024-11-16 09:32:00 +0530

    Update sprint101-150 Added the solution for a Star Pattern with Increasing Width and Centered

---
+++ sprint101-150/40.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int row;
+    cout<<"Enter the row number :" ;
+    cin>>row;
+    char lett='A' ;
+    for(int i=1 ; i<=row; i++) {
+        for(int sp=1; sp<=row-i; sp++) {
+            cout<<" ";
+        }
+        for(int c=1 ; c<=i; c++) {
+            cout<<"*";
+
+        }
+        for(int r=i-1; r>=1; r--) {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+//    for(int i=row-1; i>=1; i--) {
+  //      for(int sp=1; sp<=row-i; sp++) {
+    //        cout<<" ";
+      //  }
+       // for(int c=1; c<=i; c++) {
+        //   cout<<c;
+        //}
+      //  for(int r=i-1; r>=1; r--) {
+        //    cout<<r;
+ //       }
+   //     cout<<endl;
+   // }
+    return 0;
+}
