feeb218a70d0284c3126746f9ffebfa1771c6140
Ashish Biradar <127400020+ASHISH-hhhhh@users.noreply.github.com>
2024-11-16 08:56:17 +0530

    Update sprint101-150 Added the solution for a Pattern of Increasing and Decreasing Stars | diamond stars *2

---
+++ sprint101-150/24.increasingDecreasingStars|diamondPattern*2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int row;
+    cout<<"Enter the row number :" ;
+    cin>>row;
+    for(int i=1 ; i<=row; i++) {
+        for(int sp=1; sp<=row-i; sp++) {
+            cout<<" ";
+        }
+        for(int c=1 ; c<=i; c++) {
+            cout<<"*";
+        }
+        for(int r=i-1; r>=1; r--) {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=row-1; i>=1; i--) {
+        for(int sp=1; sp<=row-i; sp++) {
+            cout<<" ";
+        }
+        for(int c=1; c<=i; c++) {
+            cout<<"*" ;
+        }
+        for(int r=i-1; r>=1; r--) {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
