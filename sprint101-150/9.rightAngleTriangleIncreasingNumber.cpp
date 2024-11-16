b6974ee860bc1ec3a511514827ec79aaf653dbc7
Ashish Biradar <127400020+ASHISH-hhhhh@users.noreply.github.com>
2024-11-16 08:18:37 +0530

    Update sprint101-150 Added the solution for right angle triangle increasing count

---
+++ sprint101-150/9.rightAngleTriangleIncreasingNumber.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int count=0;
+    int row;
+    cout<<"Enter the row number :" ;
+    cin>>row;
+    for(int i=1; i<=row; i++) {
+        for(int c=1; c<=i; c++) {
+            count=count+1;
+            cout<<count;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
